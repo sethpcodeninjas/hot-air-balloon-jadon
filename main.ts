e580cf2368f1462067cb0cd1dd17700231452144
sethpcodeninjas <79381106+sethpcodeninjas@users.noreply.github.com>
2021-03-11 17:41:11 -0500

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,6 @@ namespace SpriteKind {
     export const Background = SpriteKind.create()
     export const Mountain = SpriteKind.create()
 }
-game.onUpdateInterval(750, function() {
-    spawnSomething(randint(0, 100))
-})
 function spawnSomething (roll: number) {
     if (roll <= 2) {
         createUFO()
@@ -16,6 +13,11 @@ function spawnSomething (roll: number) {
         createBird()
     }
 }
+controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
+    balloon.startEffect(effects.fire)
+    balloon.ay = -50
+    balloon.setImage(balloonInflated)
+})
 function createAnimationArrays () {
     ufoFrames = [img`
         .........fff.........
@@ -165,6 +167,11 @@ function createAnimationArrays () {
         . . . . . . . . . . . . . . . . 
         `]
 }
+controller.anyButton.onEvent(ControllerButtonEvent.Released, function () {
+    effects.clearParticles(balloon)
+    balloon.ay = 50
+    balloon.setImage(balloonDeflated)
+})
 function createUFO () {
     if (Math.percentChance(50)) {
         ufoSpeed = 40
@@ -197,16 +204,20 @@ function createUFO () {
     ufo.y = randint(10, scene.screenHeight() - 10)
 }
 function createBird () {
-	if(Math.percentChance(50)){
+    if (Math.percentChance(50)) {
         birdSpeed = 20
         chosenAnimation = birdGoingRight
-    }
-    else {
+    } else {
         birdSpeed = -20
         chosenAnimation = birdGoingLeft
     }
-    let bird = sprites.createProjectileFromSide(chosenAnimation[0], birdSpeed, 0)
-    animation.runImageAnimation(bird, chosenAnimation, 100, true)
+    bird = sprites.createProjectileFromSide(chosenAnimation[0], birdSpeed, 0)
+    animation.runImageAnimation(
+    bird,
+    chosenAnimation,
+    100,
+    true
+    )
     bird.y = randint(12, scene.screenHeight() - 10)
 }
 function createCloud () {
@@ -252,6 +263,11 @@ function createCloud () {
     cloud.setFlag(SpriteFlag.Ghost, true)
     cloud.y = randint(0, scene.screenHeight() * 0.6)
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.setFlag(SpriteFlag.Ghost, true)
+    sprite.say("OW!", 300)
+})
 function placeMountain (leftPosition: number) {
     lastCreatedMountain = sprites.create(mountains[randint(0, mountains.length - 1)], SpriteKind.Mountain)
     lastCreatedMountain.left = leftPosition
@@ -262,56 +278,52 @@ function placeMountain (leftPosition: number) {
 }
 function createTree () {
     tree = sprites.createProjectileFromSide(img`
-        ...............cc...............
-        ............ccc65c66............
-        ............c6c55c76............
-        ...........6cc7557c66...........
-        ..........cc77777577c6..........
-        .........666c677776cc66.........
-        ........c7776c7c67657576........
-        ........ccc666c666655666........
-        ......c66cc7666667777c6766......
-        .....c777c77667667767767776.....
-        .....cc66cccc77c677cc666666.....
-        ....c6766666c7c6767677777766....
-        ...cc777666666677767777667c66...
-        .666cc6677666667777777776677666.
-        .67776677c676677777776677677776.
-        cc6666ccc67767776777776cc7767666
-        c666777667766776c776777c67776c66
-        .c6777666ccc667c676cc666667776c.
-        .cc6666766666cc66666666776cc666.
-        ...66776c666666666677667766cccc.
-        ...cc76c66766666667677667776c...
-        ...6cccc677666666776777c77666...
-        ......6ccc7c67767776c776cc......
-        ........ccc6777c67776cc6........
-        ...........cc77c67766...........
-        .............6c6666.............
-        ............ffeeeef.............
-        ..........ffeeeeeeeef...........
-        .............feeeffe............
-        ..............fef...............
-        ..............fef...............
-        ...............f................
+        ......cc66......
+        .....c6576c.....
+        ....c677576c....
+        ....cc677666....
+        ...cc6c6667cc...
+        ..6c666777cc6c..
+        ..c76666766776..
+        ..c6777777776c..
+        ..cc67777776cc..
+        .c67cc76676676c.
+        .c777666667777c.
+        .c6777777777766.
+        .cc7767776776666
+        c676cc6766666776
+        c777766666677776
+        cc7777777777776c
+        .c676777677767c.
+        ..cc667666766c..
+        ...ccc6c66ccc...
+        .....cccccc.....
+        .......ee.......
+        ......eeee......
+        .....eeeeee.....
+        .......ee.......
         `, -10, 0)
     tree.z = -5
     tree.bottom = scene.screenHeight()
-    tree.setFlag(SpriteFlag.Ghost, true)
 }
+let nearGroundCount = 0
 let tree: Sprite = null
 let cloud: Sprite = null
 let cloudImages: Image[] = []
+let bird: Sprite = null
 let chosenAnimation: Image[] = []
+let birdSpeed = 0
 let ufo: Sprite = null
 let birdGoingRight: Image[] = []
 let birdGoingLeft: Image[] = []
-let birdSpeed: number = null
 let ufoFrames: Image[] = []
 let ufoSpeed = 0
 let lastCreatedMountain: Sprite = null
 let mountains: Image[] = []
-let balloonDeflated = img`
+let balloon: Sprite = null
+let balloonInflated: Image = null
+let balloonDeflated: Image = null
+balloonDeflated = img`
     ...................
     ...................
     .......fffff.......
@@ -346,7 +358,7 @@ let balloonDeflated = img`
     ......fcccccf......
     .......fffff.......
     `
-let balloonInflated = img`
+balloonInflated = img`
     ...................
     ......fffffff......
     ....ff2222222ff....
@@ -381,7 +393,7 @@ let balloonInflated = img`
     ......fcccccf......
     .......fffff.......
     `
-let balloon = sprites.create(balloonDeflated, SpriteKind.Player)
+balloon = sprites.create(balloonDeflated, SpriteKind.Player)
 balloon.ay = 35
 balloon.z = 100
 balloon.setFlag(SpriteFlag.StayInScreen, true)
@@ -545,3 +557,28 @@ game.setDialogFrame(img`
     `)
 game.showLongText("Stay in the air as long as you can. Press any button to turn on the burner and go up!", DialogLayout.Center)
 ufoSpeed = 0
+game.onUpdate(function () {
+    info.changeScoreBy(1)
+    if (balloon.bottom >= scene.screenHeight() - 1) {
+        balloon.say("Pull up", 100)
+        nearGroundCount += 1
+        if (nearGroundCount > 50) {
+            info.changeLifeBy(-1)
+            nearGroundCount = -20
+        }
+    }
+})
+game.onUpdate(function () {
+    balloon.vy = Math.constrain(balloon.vy, -25, 25)
+})
+game.onUpdateInterval(750, function () {
+    spawnSomething(randint(0, 150))
+})
+game.onUpdateInterval(200, function () {
+	for (let mountain of sprites.allOfKind(SpriteKind.Mountain)){
+        mountain.x += 1
+        if (lastCreatedMountain.right < scene.screenWidth()){
+                placeMountain(lastCreatedMountain.right)
+        }
+    }
+})
